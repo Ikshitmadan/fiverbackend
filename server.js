89351d35b574907fc6f10084c54acac79b9b4234
ikshitmadan <ikshitmadan71@gmail.com>
2023-05-03 10:56:30 +0530

    sks:

--- server.js
+++ server.js
@@ -25,7 +25,13 @@ const connect = async () => {
 
 
 
-app.use(cors())
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
 
 
 
