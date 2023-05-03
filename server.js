46112d6e216386328e9eac091ca3ef81824d9b38
ikshitmadan <ikshitmadan71@gmail.com>
2023-05-03 10:50:54 +0530

    sks:

--- server.js
+++ server.js
@@ -23,15 +23,9 @@ const connect = async () => {
     }
   };
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://fiver-ui.onrender.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-  });
 
-app.use(cors({origin:"https://fiver-ui.onrender.com",credentials:true}))
+
+app.use(cors())
 
 
 
